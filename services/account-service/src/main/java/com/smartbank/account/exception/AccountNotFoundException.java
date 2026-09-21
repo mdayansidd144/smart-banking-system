564e60f5e3cdd511a3c627a15a8e7fe9ec3a566b
Mohd Ayan Siddiqui <ayansiddiqui1411@gmail.com>
2026-09-21 17:14:06 +0530

    created transactions

---
+++ services/account-service/src/main/java/com/smartbank/account/exception/AccountNotFoundException.java
@@ -0,0 +1,6 @@
+package com.smartbank.account.exception;
+import java.util.UUID;
+public class AccountNotFoundException extends RuntimeException {
+    public AccountNotFoundException(UUID id) {
+        super("Account not found: " + id);
+    }
---
+++ services/account-service/src/main/java/com/smartbank/account/repository/TransactionRepository.java
@@ -0,0 +1,12 @@
+package com.smartbank.account.repository;
+
+import com.smartbank.account.entity.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
+    List<Transaction> findByAccountIdOrderByCreatedAtDesc(UUID accountId);
+}
